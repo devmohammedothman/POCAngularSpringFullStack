d705907e44f50cc0ff865360a9586ec7bdf88998
asaad <asaad@dev-asaad.TradeNET.COM>
2018-04-24 08:57:45 +0200

    Add localization to client project

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -6,22 +6,38 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { AppRoutingModule } from './app.routing.module';
 import {UserService} from './user/user.service';
-import {HttpClientModule} from "@angular/common/http";
+//import {HttpClientModule} from "@angular/common/http";
 import {AddUserComponent} from './user/add-user.component';
 
+// import ngx-translate and the http loader
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserComponent,
-    AddUserComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [UserService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        UserComponent,
+        AddUserComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
+    ],
+    providers: [UserService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
